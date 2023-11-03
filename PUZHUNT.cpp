ce2e41886f34b395c0da2c5e909ff7f260fd790b
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-03 15:23:32 +0530

    Create PUZHUNT.cpp

---
+++ PUZHUNT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	if(N>=6 && N<=8)
+	{
+	    cout<<"yes";
+	}
+	else
+	{
+	    cout<<"no";
+	}
+	return 0;
+}
