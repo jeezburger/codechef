5472108c5ef0d82b8a0e96f62ddfb945a7fa0ccf
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-03 15:12:10 +0530

    Create TOP10.cpp

---
+++ TOP10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T, X;
+	cin >> T;
+	while (T--)
+	{
+	    cin >> X;
+	    if (X <= 10)
+	    {
+	        cout << "YES\n";
+	    }
+	    else {
+	        cout << "NO\n";
+	    }
+	    
+	}
+	
+	return 0;
+}
