4a9acd20766c6899085d6556d90835fbe56b1bdd
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-03 15:17:24 +0530

    Create NEWSPAPER.cpp

---
+++ NEWSPAPER.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X;
+	cin >> X;
+	if (X >= 8 && X <= 10)
+	{
+	    cout << "YES";
+	}
+	else 
+	{
+	    cout << "NO";
+	}
+	return 0;
+}
