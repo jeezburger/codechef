375873d7ce678802ec81f40f6cf35cedbc834165
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-12 12:48:49 +0530

    Add files via upload

---
+++ TAXSAVING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T, X, Y;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X >> Y;
+	    cout << X - Y << endl;
+	}
+	return 0;
+}
