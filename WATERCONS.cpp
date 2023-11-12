1ef26b38bd0374ac47f374c27804be3051b81fdc
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-12 13:16:58 +0530

    Add files via upload

---
+++ WATERCONS.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T, X;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X;
+	    if(X>=2000)
+	    cout << "yes" << endl;
+	    else
+	    cout << "no" << endl;
+	}
+	return 0;
+}
