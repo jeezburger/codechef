064e691af7b48f0570d5c46f21a335cc98d0fce6
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-15 00:47:25 +0530

    Add files via upload

---
+++ LUDO.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T, X;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X;
+	    if(X==6)
+	    cout << "Yes" << endl;
+	    else
+	    cout << "No" << endl;
+	}
+	return 0;
+}
