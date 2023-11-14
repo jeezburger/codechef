f7edc9c1aecf7df985ea382563fe2632388cb0a1
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-15 00:47:37 +0530

    Add files via upload

---
+++ FIT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T, X;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X;
+	    cout << 2*X*5 << endl;
+	}
+	return 0;
+}
