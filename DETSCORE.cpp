6a3580b6241d2b8d717397a9b83637c5b98c6de6
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-11-17 17:14:03 +0530

    Add files via upload

---
+++ DETSCORE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T, X, Y;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X >> Y;
+	    cout << (X/10)*Y << endl;
+	}
+	return 0;
+}
