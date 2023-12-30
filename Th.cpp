537002c6748e87f357a6ed54712cf1eb26fce2e6
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-12-30 19:53:08 +0530

    Add files via upload

---
+++ Th.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin >> N;
+	if(N==7)
+	{
+	    cout << "THALA";
+	}
+	else{
+	    cout << "SADGE";
+	}
+
+}
